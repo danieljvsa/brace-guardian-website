ce68009e8331c6439111c39c85ca5b7c4788ac34
danieljvsa <danielviana18@gmail.com>
2022-05-15 18:34:38 +0100

    Website pre-finished

--- js/carrousel.js
+++ js/carrousel.js
@@ -1,5 +1,8 @@
 let slideIndex = 0;
+let clicks = 0;
 showSlides();
+showMenu()
+sendEmail()
 
 function showSlides() {
   let i;
@@ -11,4 +14,32 @@ function showSlides() {
   if (slideIndex > slides.length) {slideIndex = 1}
   slides[slideIndex-1].style.display = "block";
   setTimeout(showSlides, 5000); // Change image every 2 seconds
+}
+
+function showMenu() {
+  clicks++
+  var x = document.querySelector(".menu");
+  var y = document.querySelector(".slideshow-container");
+  console.log(clicks)
+  if (clicks % 2 == 1) {
+    x.setAttribute('style', 'height: auto !important;')
+   
+  } else {
+    x.setAttribute('style', 'height: 0 !important;')
+    
+  }
+}
+
+function sendEmail() {
+  let name = document.getElementById("name")
+  let sender = document.getElementById("email")
+  let content = document.getElementById("message")
+  console.log(name.innerHTML)
+  var link = "mailto:bracegiuardian@gmail.com"
+             + "?cc=" + sender.value +".com"
+             + "&subject=" + encodeURIComponent(name.value)
+             + "&body=" + encodeURIComponent(content.value)
+    ;
+    
+    window.location.href = link;
 }
